9fbefef050b23f1af06fbc16c0ae83be619bc223
Ajay Nara <33077370+ajay2696@users.noreply.github.com>
2018-05-29 21:56:23 -0400

    Feat:Decks Key Change

--- util/initialData.js
+++ util/initialData.js
@@ -1,34 +1,10 @@
 import {AsyncStorage} from 'react-native';
 
-export const UDACITY_DECKS ='UDACITY_DECK:DeckList'
+export const UDACITY_DECKS ='UDACITY_DECK:DeckListNew'
 
 export function setInitialData(){
-    let udacityDeck={
-        React: {
-            title: 'React',
-            questions: [
-                {
-                    question: 'What is React?',
-                    answer: 'A library for managing user interfaces'
-                },
-                {
-                    question: 'Where do you make Ajax requests in React?',
-                    answer: 'The componentDidMount lifecycle event'
-                }
-            ]
-        },
-        JavaScript: {
-            title: 'JavaScript',
-            questions: [
-                {
-                    question: 'What is a closure?',
-                    answer: 'The combination of a function and the lexical environment within which that function was declared.'
-                }
-            ]
-        }
-    };
+    let udacityDeck={};
     AsyncStorage.setItem(UDACITY_DECKS,JSON.stringify(udacityDeck));
-
     return udacityDeck;
 }
 export  function getDecksListHelper(results){
